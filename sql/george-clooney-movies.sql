25d727fcf3292b3a318d7f8bf974f41006166ff6
Vincent Antaki <antakivi@frontal04.iro.umontreal.ca>
2015-04-14 09:15:46 -0400

    removed junk from george clooney

--- sql/george-clooney-movies.sql
+++ sql/george-clooney-movies.sql
@@ -1,13 +1,4 @@
---George Clooney est realisateur
-
- (select id as i from Personne where prenom='George' and nom='Clooney');
-  
-        (select id from Realisateur where Realisateur.id in
-           (select id as i from Personne where prenom='George' and nom='Clooney'));
-
-    (select noVideo from Realise where Realise.id in
-        (select id from Realisateur where Realisateur.id in
-           (select id as i from Personne where prenom='George' and nom='Clooney')));
+--George Clooney est realisateur, affiche les film realiser par george clooney
 select titre from Video where id in
     (select noVideo from Realise where Realise.id in
         (select id from Realisateur where Realisateur.id in
