92d4fee565ba3a02880f0ee63e73d4f864efa194
Emile Trottier <trottiee@blm46.iro.umontreal.ca>
2015-04-14 09:15:16 -0400

    fix cinenam-projet

--- sql/cinema-projet.sql
+++ sql/cinema-projet.sql
@@ -1,8 +1,8 @@
 -- Afficher toutes les projections d'un film avec George Clooney dans les acteurs, y compris les projections ayant deja eu lieu. 
 Select * 
 From (Cinema join 
-        (Select noCinema as nc, noVideo as nv FROM Projette WHERE nv IN 
+        (Select noCinema as nc, noVideo as nv FROM Projette WHERE Projette.noVideo IN 
             (Select noVideo From APourRole where APourRole.id IN
                 (Select id From Acteur where Acteur.id IN 
                     (Select id as i From Personne WHERE prenom='George' AND nom='Clooney')))) 
-            on Cinema.noCinema = nc); 
+            on Cinema.id = nc); 
